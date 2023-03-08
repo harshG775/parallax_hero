33cba8efdc01c7ef9fd3c6a3841b4bcc5602dff6
harshG775 <hgaur491@gmail.com>
2023-03-08 21:46:50 +0530

    simple paralax hero section with navbar

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+let btn=document.querySelector(".menu_btn");
+let navlinks=document.querySelector(".navlinks");
+btn.addEventListener("click",function() {
+    navlinks.classList.toggle("toggle")
+})
